35d94011e379702cab2e958b5186ac9b3f0df106
--replace-all <loretodoris68@gmail.com>
2026-05-12 12:10:59 +0200

    feat modificación App.jsx y tracker1

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,10 @@ import './App.css'
 
 function App() {
   return (
-<<<<<<< HEAD
-    <>
-      <h1>hola gente</h1>
-      <p>Bienvenidos a nuestra aplicación de seguimiento de COVID-19</p>
-    </>
-  )
-=======
     
       <Router />
      
   );
->>>>>>> cadd8b8d2d5546cb9259d2f68fdccd9d76c7d55b
 }
 
 export default App
